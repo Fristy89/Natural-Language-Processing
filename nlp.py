f0f69db78b925a19e851e54260ebb50a6db72577
Fristy89 <dpieruschka@gmail.com>
2018-10-26 13:11:46 +0200

    dennis

---
+++ nlp.py
@@ -0,0 +1,65 @@
+# import Modules
+import nltk
+import csv
+import pandas
+
+
+bi-grams-NL = ["de", "en", ""]
+bi-grams-FR = ["Tu", "es"]
+bi-grams-DU = [""]
+bi-grams-EN = [""]
+tri-grams-NL = [""]
+tri-grams-FR = [""]
+tri-grams-DU = [""]
+tri-grams-EN = [""]
+
+
+# Functie voor het inladen van een een tekst
+
+
+
+
+test = {"bigram":test1, "trigram": test2}
+
+class translator(object):
+
+    def __init__(self, data, test**):
+        self.data = data
+        self.bigram = bigram    
+        self.trigram = trigram
+
+bestand = input("voer de naam van uw bestand in.")
+tokens = nltk.word_tokenize(bestand)
+# Functie voor het inladen van een een tekst
+
+
+    def load_data(self.data) :
+        for i in text:
+        print i 
+
+    #lees elke lijn in de file
+    file = open(“testfile.txt”, “r”) 
+    for line in file: 
+        print line
+
+
+
+
+# Functie voor de taal te herkennen
+
+def recognize_language(language, bigram, trigram ):
+
+# functie voor de output te weergeven
+
+
+
+def output():
+    print("Deze taal is")
+    return taal
+
+
+
+
+if __name__ == '__main__':
+    main()
+    
